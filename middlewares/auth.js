13d8f9e62ec781937aa488a755fb1d598aa37791
Ashif Pathan <ashifbgmiking@gmail.com>
2025-04-21 20:10:07 +0530

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -84,9 +84,9 @@ exports.isAdmin = async (req, res, next) => {
 exports.isInstructor = async (req, res, next) => {
 	try {
 		const userDetails = await User.findOne({ email: req.user.email });
-		//console.log(userDetails);
+		console.log(userDetails);
 
-		//console.log(userDetails.accountType);
+		console.log(userDetails.accountType);
 
 		if (userDetails.accountType !== "Instructor") {
 			return res.status(401).json({
